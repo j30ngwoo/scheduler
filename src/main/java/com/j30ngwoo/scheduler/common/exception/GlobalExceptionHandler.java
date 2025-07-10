d7b572f32dd6f87bcf5de792c0fe10a7445f19ba
j30ngwoo <ttangle1234@naver.com>
2025-07-11 03:52:32 +0900

    exception 발생 시 로그 추가

--- src/main/java/com/j30ngwoo/scheduler/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/j30ngwoo/scheduler/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.j30ngwoo.scheduler.common.exception;
 
 import com.j30ngwoo.scheduler.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +21,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleUnhandled(Exception e) {
+        Logger logger = LoggerFactory.getLogger(getClass());
+        logger.error("서버 예기치 못한 오류 발생", e);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error("INTERNAL_SERVER_ERROR", "예기치 못한 오류가 발생했습니다."));
