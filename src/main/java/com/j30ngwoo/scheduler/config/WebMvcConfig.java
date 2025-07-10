af6f3b1ae98de8816b2f6de29b3b28ce4548f82f
j30ngwoo <ttangle1234@naver.com>
2025-07-11 03:49:24 +0900

    cors 설정 추가

--- src/main/java/com/j30ngwoo/scheduler/config/WebMvcConfig.java
+++ src/main/java/com/j30ngwoo/scheduler/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import com.j30ngwoo.scheduler.config.resolver.CurrentUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,5 +33,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
